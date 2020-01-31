8760140116b597d24729c512be6636036883c415
jwilly117 <jakewilliams117@gmail.com>
2020-01-30 19:56:22 -0500

    adding database references

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
-alert("Hello");
+
+// Create a variable to reference the firebase
+var database = firebase.database();
+
+// ConnectionsRef to reference all the connections. 
+var connectionsRef = database.ref("/connections");
+
+// .info connected is a special location updatad when 
+// any clients connection boolean changes.
+var connectedRef = database.ref(".info/connected");
